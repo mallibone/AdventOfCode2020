f1c48cf7e3fbade12e9323e35b8f26fc540f58f4
Mark Allibone <mallibone@outlook.com>
2020-12-09 15:33:18 +0100

    Solves part 1 of day 7

---
+++ 07/notepad.fsx
@@ -0,0 +1,43 @@
+#time
+open System
+open System.IO
+open System.Text.RegularExpressions
+
+let parseContainedBags containedBags =
+    Regex.Matches(containedBags, "(\d+)\s(\w+)(\s?)(\w*)")
+    |> Seq.map (fun m -> m.Value)
+    |> Seq.toList
+    |> List.map (fun m -> (Convert.ToInt32(Regex.Match(m, "^(\d+)").Value), Regex.Match(m, "(\w+)(\s?)(\w*)$").Value))
+
+let getBagDefinitions (bagDefinition:string) =
+    bagDefinition.Split(" bags contain ")
+    |> (fun lines -> (lines.[0], (parseContainedBags lines.[1])))
+
+let rec findPossibleContainers (bagToStow:string) (bagDefinitions:list<string * list<int * string>>) =
+    // bagDefinitions |> Seq.filter (fun bd -> (bd |> Seq.exists (fun bi -> bi = bagToStow)))
+    bagDefinitions 
+    |> List.filter (fun (_, bd) -> (bd |> List.exists (fun (_, bi) -> bi = bagToStow)))
+    |> List.map (fun (bag, _) -> bag)
+    |> List.collect (fun bag -> [bag] |> List.append (findPossibleContainers bag bagDefinitions))
+    |> List.distinct
+
+let rec containingBags parentBag sum (bagDefinitions:list<string * list<int * string>>) =
+    let childBags = bagDefinitions |> List.filter(fun (bag, _) -> bag = parentBag) |> List.collect (fun (_, containedBags) -> containedBags)
+    match childBags with
+    | [] -> sum
+    | _ -> 
+        let newSum = sum + (childBags |> List.sumBy (fun (count, _) -> count))
+        childBags |> List.reduce (fun (_, name) -> (containingBags name newSum bagDefinitions))
+
+// File.ReadLines("testInput.txt")
+File.ReadLines("input.txt")
+|> Seq.toList
+|> List.map getBagDefinitions
+|> findPossibleContainers "shiny gold"
+|> List.length
+
+// part 2
+File.ReadLines("input.txt")
+|> Seq.toList
+|> List.map getBagDefinitions
+|> containingBags "shiny gold" 0
