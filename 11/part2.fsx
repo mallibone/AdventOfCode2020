af969432774a7c5a3961736a6512600388ff0a05
Mark Allibone <mallibone@outlook.com>
2020-12-13 16:25:54 +0100

    Cleanup

--- 11/part2.fsx
+++ 11/part2.fsx
@@ -12,12 +12,9 @@ let rec sumNeighbours currentRow currentColumn (currentState:string array array)
     let maxY = (currentState.Length)
 
     let rec findSeat currentX currentY (deltaX,deltaY) =
-        // let newX = if (currentX = minX) || (currentX = maxX) then currentX else currentX + deltaX
-        // let newY = if (currentY = minY) || (currentY = maxY) then currentY else currentY + deltaY
         let newX = currentX + deltaX
         let newY = currentY + deltaY
-        // printfn "X: %d Y: %d" newX newY
-        // Console.ReadLine() |> ignore
+
         if (newX >= maxX) || (newX < minX) || (newY >= maxY) || (newY < minY) then 
             if (currentX = currentColumn) && (currentY = currentRow) then "" else currentState.[currentY].[currentX]
         elif currentState.[newY].[newX] = "." then 
@@ -29,11 +26,9 @@ let rec sumNeighbours currentRow currentColumn (currentState:string array array)
                                    (-1,0);       (1,0);
                                    (-1,1);(0,1); (1,1)|]
 
-    // let sum = findNeighoursOperation |> Array.map (findSeat currentColumn currentRow)
-    //                                  |> Array.sumBy (fun place -> if place = "#" then 1 else 0)
-    let sum = findNeighoursOperation |> Array.map (findSeat currentColumn currentRow)
-                                     |> Array.sumBy (fun place -> if place = "#" then 1 else 0)
-    sum
+    findNeighoursOperation 
+    |> Array.map (findSeat currentColumn currentRow)
+    |> Array.sumBy (fun place -> if place = "#" then 1 else 0)
 
 let rec runRound currentRow currentColumn (nextState:string array array) (currentState:string array array) =
 
@@ -59,15 +54,12 @@ let rec runRound currentRow currentColumn (nextState:string array array) (curren
 
 let rec playGame (currentState:string array array) =
     let newState = (Array.zeroCreate currentState.Length |> Array.map (fun _ -> Array.zeroCreate currentState.[0].Length |> Array.map (fun _ -> ".")))
-    printfn "%A" currentState
-    // Console.ReadLine() |> ignore
-    // let gna = (Array2D.create currentState.Length currentState.[0].Length  "")
     match runRound 0 0 newState currentState with
     | state when state = currentState -> currentState
     | newState -> playGame newState
 
 #time
 
-loadSeatingArea "sampleInput.txt"
+loadSeatingArea "input.txt"
 |> playGame
 |> Array.collect(fun a -> a) |> Array.sumBy (fun s -> if s = "#" then 1 else 0)
